20dc88db6095c8ce87ca07d6fa4cbb012ee12ad4
Alejo Granero <alejogranero@gmail.com>
2019-02-12 16:49:05 -0300

    Primer commit desde Lenovo

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports : [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ]
+})
+
+export class PagesModule { }
